7283dd0c27e9abc6f4d052e130341e2950022304
Andreas Eymiller <github@eymiller.de>
2018-07-28 11:23:27 +0200

    First version

---
+++ HelloWorld/src/de/eymiller/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package de.eymiller.helloworld;
+
+/**
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * 
+   * @param arg
+   */
+  public static void main(String... arg) {
+    System.out.println("Hello world");
+  }
+}
