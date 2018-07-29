5d166f484a2d0ac4e40d3d480900d680837e95b9
Andreas Eymiller <github@eymiller.de>
2018-07-29 08:37:57 +0200

    Change

--- HelloWorld/src/de/eymiller/helloworld/HelloWorld.java
+++ HelloWorld/src/de/eymiller/helloworld/HelloWorld.java
@@ -10,6 +10,6 @@ public class HelloWorld {
    * @param arg
    */
   public static void main(String... arg) {
-    System.out.println("Hello world");
+    System.out.println("Hello world 1");
   }
 }
